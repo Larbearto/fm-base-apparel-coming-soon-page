055901f0d63252b09a8391a8fa039250edbfccf9
SpiderBear <SpiderBear714@gmail.com>
2022-11-19 22:28:35 -0800

    responsive layout and desktop layout complete

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
-let btn = document.querySelector(".btn")
-let email = document.querySelector("input[type=text]")
-let errP = document.querySelector("#error")
-let errIcon = document.querySelector("#error-icon")
-
-btn.addEventListener("click", (e) => {
-  e.preventDefault()
-  if (
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-      email.value
-    )
-  ) {
-    email.classList.remove("error")
-    errIcon.classList.remove("error")
-    errP.classList.remove("error")
-    return true
-  } else {
-    email.classList.add("error")
-    errIcon.classList.add("error")
-    errP.classList.add("error")
-    setTimeout(() => {
-      email.classList.remove("error")
-      errIcon.classList.remove("error")
-      errP.classList.remove("error")
-    }, 3000)
-    return false
-  }
-})
+const submitBtn = document.getElementById('btn')
+const errorMsg = document.querySelector('.message')
+const errorIcon = document.querySelector('.error-icon')
+const inputEl = document.querySelector('.emailInput')
+const emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
